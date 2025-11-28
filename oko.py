edebaed8c6851b0b4fd2321b015374cb2fbb2d98
kmashinist <konstantinmashinistov1310@gmail.com>
2025-11-28 18:50:55 +0500

    the second shameful thread after everything broke

---
+++ oko.py
@@ -0,0 +1,28 @@
+from Aruca import ArucoDetector
+import cv2
+import numpy as np
+import yaml
+import sys
+
+class AllSeeEye:
+    def __init__(self, path_yaml):
+        with open("config.yaml", 'r') as file:
+            data = yaml.safe_load(file)
+
+        self.camera_id = data['camera_id']
+        self.cap = cv2.VideoCapture(self.camera_id)
+        if data['calibration_file'] is not None:
+            self.path2npz = data['calibration_file']
+        else:
+            print('Warning: no calibration npz file', file=sys.stderr)
+        self.detector = ArucoDetector(path_yaml)
+
+    def get_pos(self, ):
+        ret, frame = self.cap.read()
+        all_poses = self.detector.get_pos(frame)
+        return all_poses
+
+    def debug_draw(self):
+        ret, frame = self.cap.read()
+        debug_frame = self.detector.draw(frame)
+        cv2.imshow("Aruco Tracker", debug_frame)
